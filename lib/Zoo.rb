2624fd73b552809e9909d918a114f0c65891299c
Reina <r.clarkemitchell@gmail.com>
2019-09-19 13:55:14 -0400

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,31 @@
 class Zoo
+    attr_reader :location, :name
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location 
+        @@all << self
+    end 
+
+    def self.all
+    @@all
+    end
+
+# def iteration(attr)
+#     Animal.all.map {|animal| animal.attr}
+# end
+
+    def animal_species
+    Animal.all.map {|animal| animal.species}.uniq
+    end
+
+    def animal_nicknames
+    Animal.all.map {|animal| animal.nickname}
+    
+    end
+    def self.find_by_location(location)
+        Zoo.all.select {|zoos| zoos.location}
+    end
 
 end
