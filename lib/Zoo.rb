74cc6766f27c4ac5e481c2cfdbe7d7b835080950
Alex Farmer <farmale10@gmail.com>
2019-09-19 18:42:34 +0000

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,51 @@
+# ///// A `zoo` should be initialized with a name and a location, which should both be passed as strings.
+# ///// `Zoo#location` should return the location of the zoo instance.
+# ///// `Zoo#name` should return the name of the zoo instance.
+# ///// `Zoo.all` should return an array of all the zoo instances.
+# ///// `Zoo#animals` should return all the animals that a specific instance of a zoo has.
+# ///// `Zoo#animal_species` should return an array of all the species (as strings) of the animals in the zoo. However, if you have two dogs, it should only return one "Dog" string (aka an **unique** array).
+# ///// `Zoo#animal_nicknames` should return an array of all the nicknames of animals that a specific instance of a zoo has.
+# ///// `Zoo#find_by_species` should take in an animal's species as an argument and return an array of all the animals in that zoo, which are of that species.
+# ///// `Zoo.find_by_location` should take in a location as an argument and return an array of all the zoos within that location.
+
 class Zoo
 
+    attr_reader :name, :location
+    attr_accessor :my_animals
+
+    @@all = [] #all instances of Zoo
+
+    def initialize(name, location)
+        @name = name 
+        @location = location
+        @@all.push(self)
+        @my_animals = []
+    end 
+
+    def self.all 
+     @@all
+    end 
+
+   def animals
+    @my_animals
+   end 
+
+
+    def self.find_by_location(my_location)
+     self.all.select { |zoo| zoo.location == my_location }
+    end 
+
+    def animal_species
+     @my_animals.map { |animal| animal.species }.uniq
+    end 
+
+    def find_by_species(species_type)
+     @my_animals.select { |animal| animal.species == species_type }
+    end 
+
+    def animal_nicknames
+     animals.map { |animal| animal.nickname }
+    end 
+
 end
+
--- run.rb
+++ run.rb
@@ -5,6 +5,13 @@ require 'pry'
 
 #Test your code here
 
+westchester = Zoo.new("Westchester Zoo", "White Plains, NY")
+brooklyn = Zoo.new("Brooklyn Zoo", "Brooklyn, NY")
+bronx = Zoo.new("Bronx Zoo", "The Bronx, NY")
+
+tiger = Animal.new("Tiger", 400, "Tigger")
+lion = Animal.new("Lion", 600, "Simba")
+bear = Animal.new("Bear", 800, "Yogi")
 
 binding.pry
 puts "done"
