13902e674b6586f67dc9e92f9cf035f670cb1a88
Alex Farmer <farmale10@gmail.com>
2019-09-19 19:41:59 +0000

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,15 +1,8 @@
-# An `animal` should be instantiated with the species (e.g. "Cat", "Dog", "Rat"), a numerical weight and a nickname. Keep in mind that the animal's species and nickname should not be able to change, but its weight can.
-# `Animal#nickname` should return the nickname of the animal.
-# `Animal#weight` should return the weight of the animal.
-# `Animal#species` should return the species of the animal.
-# `Animal.all` should return an array of all the animal instances.
-# `Animal#zoo` should return the zoo instance that the instance belongs to.
-# `Animal.find_by_species` should take in an animal's species as an argument and return an array of all the animals, which are of that species.
 
 class Animal
 
     attr_reader :species, :nickname
-    attr_accessor :weight
+    attr_accessor :weight, :zoo
 
     @@all = []
 
@@ -17,7 +10,7 @@ class Animal
       @species = species
       @weight = weight
       @nickname = nickname
-      push_animal_to_zoo(zoo, self)
+      @zoo = zoo
       @@all.push(self)
     end
 
@@ -26,17 +19,11 @@ class Animal
     end
 
     def zoo
-     Zoo.all.select { |zoo| zoo.my_animals.include?(self) }
+     @zoo
     end
 
     def self.find_by_species(species_type)
       self.all.select { |animal| animal.species == species_type }
-    end
-
- private
-
-    def push_animal_to_zoo(zoo, animal_to_push)
-        zoo.my_animals.push(animal_to_push)
     end
 
  end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,17 +1,8 @@
-# ///// A `zoo` should be initialized with a name and a location, which should both be passed as strings.
-# ///// `Zoo#location` should return the location of the zoo instance.
-# ///// `Zoo#name` should return the name of the zoo instance.
-# ///// `Zoo.all` should return an array of all the zoo instances.
-# ///// `Zoo#animals` should return all the animals that a specific instance of a zoo has.
-# ///// `Zoo#animal_species` should return an array of all the species (as strings) of the animals in the zoo. However, if you have two dogs, it should only return one "Dog" string (aka an **unique** array).
-# ///// `Zoo#animal_nicknames` should return an array of all the nicknames of animals that a specific instance of a zoo has.
-# ///// `Zoo#find_by_species` should take in an animal's species as an argument and return an array of all the animals in that zoo, which are of that species.
-# ///// `Zoo.find_by_location` should take in a location as an argument and return an array of all the zoos within that location.
 
 class Zoo
 
     attr_reader :name, :location
-    attr_accessor :my_animals
+    attr_accessor 
 
     @@all = [] #all instances of Zoo
 
@@ -19,7 +10,6 @@ class Zoo
         @name = name
         @location = location
         @@all.push(self)
-        @my_animals = []
     end
 
     def self.all
@@ -27,7 +17,7 @@ class Zoo
     end
 
    def animals
-    @my_animals
+    Animal.all.find { |animal| animal.zoo == self }
    end
 
     def self.find_by_location(my_location)
@@ -35,16 +25,16 @@ class Zoo
     end
 
     def animal_species
-     @my_animals.map { |animal| animal.species }.uniq
+     animals.map { |animal| animal.species }.uniq
     end
 
     def find_by_species(species_type)
-     @my_animals.select { |animal| animal.species == species_type }
+     animals.select { |animal| animal.species == species_type }
     end
 
     def animal_nicknames
      animals.map { |animal| animal.nickname }
     end
-    
+
  end
 
