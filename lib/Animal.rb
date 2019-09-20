660ee01bdb74da4609dcf1047f9fba913678df63
solomonp90 <solomonraulpena@gmail.com>
2019-09-19 21:30:00 -0400

    initialize commit

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,26 @@
-class Animal
+class Animal    
+    attr_reader :species,:nickname
+    attr_accessor :weight,:zoo
+    @@all=[]
+    # @@species=[]
+def initialize(zoo,species,weight,nickname)
+    @zoo=zoo
+    @species=species
+    @weight=weight
+    @nickname=nickname
+    @@all << self
+    # @@species << self.species
+    
+end
+def self.all
+    @@all
+end
+
+def self.find_by_species(species)
+    self.all.select do |animal|
+        animal.species == species
+    end
+end
+
 
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,50 @@
+
+
+
+
+
 class Zoo
+attr_reader :name,:location
+@@all =[]
+def initialize(name,location)
+    @name=name
+    @location=location
+    @@all<< self
+end
+
+def self.all
+    @@all
+end
+
+def animals
+    Animal.all.select do |animal|
+        animal.zoo == self
+  end
+end
+
+def animal_species
+    self.animals.map do |animal|
+         animal.species
+    end
+end
+
+def self.find_by_location(location)
+    self.all.select do |zoo|
+        zoo.location==location
+    end
+end
+
+def find_by_species(species)
+    self.animals.select do |animal|
+        animal.species==species
+          
+    end
+end
+
+def find_by_nickname
+    self.animals.map do |animal|
+        animal.nickname
+    end   
+end
 
 end
