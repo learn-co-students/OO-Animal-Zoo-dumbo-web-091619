ce27b03154b5418e68b5bd25be5e38d772ad2dbc
solomonp90 <solomonraulpena@gmail.com>
2019-09-21 19:31:31 -0400

    initialize commit

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,7 +1,11 @@
-
-
-
-
+# - `Zoo#location` should return the location of the zoo instance. -->
+#  - `Zoo#name` should return the name of the zoo instance. -->
+#  - `Zoo.all` should return an array of all the zoo instances. -->
+#  - `Zoo#animals` should return all the animals that a specific instance of a zoo has. -->
+#  - `Zoo#animal_species` should return an array of all the species (as strings) of the animals in the zoo. However, if you have two dogs, it should only return one "Dog" string (aka an **unique** array). -->
+# - `Zoo#find_by_species` should take in an animal's species as an argument and return an array of all the animals in that zoo, which are of that species.
+# - `Zoo#animal_nicknames` should return an array of all the nicknames of animals that a specific instance of a zoo has.
+# - `Zoo.find_by_location` should take in a location as an argument and return an array of all the zoos within that location.
 
 class Zoo
 attr_reader :name,:location
